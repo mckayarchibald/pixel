b0a62d533445ad76c102b156dc284a6f9977cb1e
McKay Archibald <mck.archibald@gmail.com>
2022-02-01 22:01:58 -0700

    fix issue with initial color

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import ColorPalette from './components/ColorPalette';
 function App() {
   const [row, setRow] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
   const [column, setColumn] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
-  const [paintColor, setPaintColor] = useState('#00000')
+  const [paintColor, setPaintColor] = useState('#000000');
 
   return (
     <div>
--- client/src/components/Pixel.js
+++ client/src/components/Pixel.js
@@ -3,15 +3,7 @@ import { useState } from 'react';
 export default function Pixel({paintColor}) {
   const [color, setColor] = useState('#FFFFFF');
 
-  const handleClick = () => {
-    if(color === '#FFFFFF') {
-      setColor(paintColor);
-    } else {
-      setColor('#FFFFFF');
-    }
-  }
-
   return (
-    <button style={{height: '32px', width: '32px', backgroundColor: `${color}`, border: '.5px dashed #dfdfdf', color: `${color}`}} onClick={handleClick}>0</button>
+    <button style={{height: '32px', width: '32px', backgroundColor: `${color}`, border: '.5px dashed #dfdfdf', color: `${color}`}} onClick={() => setColor(paintColor)}>0</button>
   )
 }
